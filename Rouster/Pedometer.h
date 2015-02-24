6a4d99f30c4ce72371331661a2ce13d488e200c8
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-24 10:22:23 -0800

    Adding Pedometer class

---
+++ Rouster/Pedometer.h
@@ -0,0 +1,13 @@
+//
+//  Pedometer.h
+//  Rouster
+//
+//  Created by Rodrigo Carballo on 2/24/15.
+//  Copyright (c) 2015 Eric Mentele. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pedometer : NSObject
+
+@end
